3089c5bb65d01bab96cbbec7eaa56517ca2dbf96
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-03 16:02:29 +0200

    clean up physionet script

--- src/bin/physionet.jl
+++ src/bin/physionet.jl
@@ -1,6 +1,7 @@
 ################################################################################
 
 using MindReader
+using HiddenMarkovModelReaders
 
 ################################################################################
 
@@ -17,186 +18,132 @@ end;
 
 ################################################################################
 
+# hidden Markov model parameters
+hmmParams = HMMParams(pen = 200., distance = euclDist)
+
+################################################################################
+
 #  argument parser
 include( "Utilities/argParser.jl" );
 
 ################################################################################
 
-#  function main()
+outsvg = "/Users/drivas/Factorem/MindReader/data/svg/"
+outcsv = "/Users/drivas/Factorem/MindReader/data/csv/"
+outscreen = "/Users/drivas/Factorem/MindReader/data/screen/"
+outhmm = "/Users/drivas/Factorem/MindReader/data/hmm/"
+
+dir = "/Users/drivas/Factorem/EEG/data/physionet.org/files/chbmit/1.0.0/chb04/"
+xfile = "chb04-summary.txt"
+# file = "chb04_28.edf"
+
+winBin = 256
+overlap = 4
+
+annotFile = annotationReader( string(dir, xfile) )
 
 ################################################################################
 
-#  declare tool directories
-begin
-  utilDir    = "Utilities/"
-  montageDir = "Montage/"
-  annotDir   = "Annotator/"
-  signalDir  = "SignalProcessing/"
-  arqDir     = "Architect/"
-  hmmDir     = "HiddenMarkovModel/"
-  pcaDir     = "PrincipalComponentAnalysis/"
-  imgDir     = "ImageProcessing/"
-  graphDir   = "Graphics/"
-  performDir = "Performance/"
-end;
+dirRead = readdir(dir)
+fileList = contains.(dirRead, r"edf$") |> p -> getindex(dirRead, p)
 
-################################################################################
+for file in fileList
 
-#  load functions
-begin
-  @info("Loading modules...")
-  include( string(utilDir,    "fileReaderEDF.jl") )
-  include( string(montageDir, "electrodeID.jl") )
-  include( string(performDir, "stateStats.jl") )
-  include( string(annotDir,   "fileReaderXLSX.jl") )
-  include( string(annotDir,   "annotationCalibrator.jl") )
-  include( string(signalDir,  "signalBin.jl") )
-  include( string(signalDir,  "fastFourierTransform.jl") )
-  include( string(hmmDir,     "hiddenMarkovModel.jl") )
-  include( string(arqDir,     "architect.jl") )
-  include( string(arqDir,     "shapeShifter.jl") )
-  include( string(arqDir,     "autoencoder.jl") )
-  # include( string(arqDir,     "SMPerceptron.jl") )
-  include( string(graphDir,   "statesHeatMap.jl") )
-  include( string(performDir, "screening.jl") )
-  include( string(performDir, "permutations.jl") )
-end;
+  @info file
+  outimg = replace(file, ".edf" => "")
 
-################################################################################
+  #  read data
+  begin
+    # read edf file
+    edfDf, startTime, recordFreq = getSignals( string(dir, file) )
 
-#  read data
-begin
-  # read edf file
-  edfDf, startTime, recordFreq = getSignals(file)
+    if haskey(annotFile, outimg)
 
-  # # read xlsx file
-  # xfile = replace(file, "edf" => "xlsx")
-  # xDf = xread(xfile)
+      labelAr = annotationCalibrator(
+        annotFile[outimg],
+        startTime = startTime,
+        recordFreq = recordFreq,
+        signalLength = size(edfDf, 1),
+        binSize = winBin,
+        binOverlap = overlap
+      )
 
-  # # labels array
-  # labelAr = annotationCalibrator(
-    # xDf,
-    # startTime = startTime,
-    # recordFreq = recordFreq,
-    # signalLength = size(edfDf, 1),
-    # binSize = winBin,
-    # binOverlap = overlap
-  # )
-
-  # calculate fft
-  freqDc = extractChannelFFT(edfDf, binSize = winBin, binOverlap = overlap)
-end;
-
-################################################################################
-
-# build autoencoder & train hidden Markov model
-begin
-  for d in [Symbol(i, "Dc") for i = [:err, :post, :comp]]
-    @eval $d = Dict{String, Tuple{Array{Int64, 1}, Array{Array{Float64, 1}, 1}}}()
-  end
-
-  # TODO: check errors. Array
-  # ssDc = Dict()
-  for (k, f) in freqDc
-    println()
-    @info k
-    # ssDc[k] = Dict()
-
-    #  build & train autoencoder
-    freqAr = shifter(f)
-    # model = buildDeepRecurrentAutoencoder(length(freqAr[1]), 100, leakyrelu)
-    # model = buildRecurrentAutoencoder(length(freqAr[1]), 100, leakyrelu)
-    model = buildAutoencoder(length(freqAr[1]), 100, leakyrelu)
-    model = modelTrain(freqAr, model, Params)
-
-    ################################################################################
-
-    #  # post
-    postAr = cpu(model).(freqAr)
-    #  aPos = reshifter(postAr) |> p -> Flux.flatten(p)
-    #
-    #  # setup
-    #  mPen, hmm = setup(aPos)
-    #  # process
-    #  for i in 1:5
-    #    postDc[k] = process(hmm, aPos, mPen)
-    #  end
-    #
-    #  # # calculate sensitivity & specificity
-    #  # ssDc[k]['P'] = sensspec(postDc[k][1], labelAr)
-
-    ################################################################################
-
-    # error
-    aErr = reshifter(postAr - freqAr) |> p -> Flux.flatten(p) |> permutedims
-
-    # setup
-    mPen, hmm = setup(aErr)
-
-    # process
-    for i in 1:4
-      errDc[k] = process(hmm, aErr, mPen, true)
     end
 
-    # final
-    for i in 1:2
-      errDc[k] = process(hmm, aErr, mPen, false)
+    # calculate fft
+    freqDc = extractChannelFFT(edfDf, binSize = winBin, binOverlap = overlap)
+  end;
+
+  ################################################################################
+
+  # build autoencoder & train hidden Markov model
+  begin
+    for d in [Symbol(i, "Dc") for i = [:err, :post, :comp]]
+      @eval $d = Dict{String, Tuple{Array{Int64, 1}, Array{Array{Float64, 1}, 1}}}()
     end
 
-    # # calculate sensitivity & specificity
-    # ssDc[k]['E'] = sensspec(errDc[k][1], labelAr)
+    for (k, f) in freqDc
+      println()
+      @info k
+
+      #  build & train autoencoder
+      freqAr = shifter(f)
+      model = buildAutoencoder(length(freqAr[1]), 100, leakyrelu)
+      model = modelTrain(freqAr, model, Params)
+
+      ################################################################################
+
+      postAr = cpu(model).(freqAr)
+
+      ################################################################################
+
+      begin
+        @info "Creating Hidden Markov Model..."
+        # error
+        aErr = reshifter(postAr - freqAr) |> p -> Flux.flatten(p) |> permutedims
+
+        # setup
+        hmm = setup!(aErr)
+
+        # process
+        for i in 1:4
+          errDc[k] = process(hmm, aErr, true, args = hmmParams)
+        end
+
+        # final
+        for i in 1:2
+          errDc[k] = process(hmm, aErr, false, args = hmmParams)
+        end
+      end;
+
+      ################################################################################
+
+    end
+  end;
+
+  ################################################################################
+
+  if haskey(annotFile, outimg)
+
+    scr = sensspec(errDc, labelAr)
+
+    DelimitedFiles.writedlm( string(outscreen, outimg, ".csv"), writePerformance(scr), ", " )
 
     ################################################################################
 
-    #  # compressed
-    #  compAr = cpu(model[1]).(freqAr)
-    #  aComp = reshifter(compAr, length(compAr[1])) |> p -> Flux.flatten(p)
-    #
-    #  # setup
-    #  mPen, hmm = setup(aComp)
-    #  # process
-    #  for i in 1:5
-    #    compDc[k] = process(hmm, aComp, mPen)
-    #  end
-    #
-    #  # # calculate sensitivity & specificity
-    #  # ssDc[k]['C'] = sensspec(compDc[k][1], labelAr)
+    runHeatmap(outimg, outsvg, outcsv, errDc, labelAr)
 
-    ################################################################################
+  else
+
+    runHeatmap(outimg, outsvg, outcsv, errDc)
 
   end
-end;
 
-################################################################################
+  ################################################################################
 
-# scree = sensspec(errDc, labelAr)
-# permut = rdPerm(errDc, labelAr, weighted = false)
-# perWeg = rdPerm(errDc, labelAr, weighted = true)
+  writeHMM( string(outhmm, replace(file, ".edf" => "_")), errDc)
 
-# function toArray(stDc::Dict{String, Array{Float64, 2}})
-  # outAr = Array{Float64, 2}(undef, length(stDc), 2)
-  # ct = 0
-  # for (_, v) in stDc
-    # ct += 1
-    # outAr[ct, :] = v
-  # end
-  # return outAr
-# end
-
-# DelimitedFiles.writedlm(string("screen/", outimg, ".csv"), toArray(scree), ", ")
-# DelimitedFiles.writedlm(string("permutation/U", outimg, ".csv"), toArray(permut), ", ")
-# DelimitedFiles.writedlm(string("permutation/W", outimg, ".csv"), toArray(perWeg), ", ")
-
-################################################################################
-
-runHeatmap(outimg, outsvg, outcsv, errDc)
-# runHeatmap(errDc, labelAr)
-
-################################################################################
-
-#  end
-
-################################################################################
+  ################################################################################
 
 end
 
