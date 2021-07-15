6ba19d87797b6a9faf76306dc35a9ea1038c320d
DanielRivasMD <danielrivasmd@gmail.com>
2021-07-15 13:39:57 +0200

    add parameters

---
+++ src/runDataset/Parameters.jl
@@ -0,0 +1,24 @@
+################################################################################
+
+# set hyperparameters
+@with_kw mutable struct NNParams
+  η::Float64                   = 1e-3             # learning rate
+  epochs::Int                  = 25               # number of epochs
+  batchsize::Int               = 1000             # batch size for training
+  throttle::Int                = 5                # throttle timeout
+  device::Function             = gpu              # set as gpu, if gpu available
+  σ::Function                  = leakyrelu        # learning function
+  λ::Int                       = 64               # hidden layer on autoencoder
+end;
+
+################################################################################
+
+# hidden Markov model parameters
+hmmParams = HMMParams(
+  penalty = 200,
+  distance = euclDist,
+  minimumFrequency = 20,
+  verbosity = false,
+)
+
+################################################################################
