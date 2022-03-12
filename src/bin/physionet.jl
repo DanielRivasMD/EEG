d29818c17d328460801ef5343959144e7f935964
DanielRivasMD <danielrivasmd@gmail.com>
2022-03-12 15:21:29 +0100

    add config & load modules

--- src/bin/physionet.jl
+++ src/bin/physionet.jl
@@ -1,25 +1,47 @@
 ################################################################################
 
-using MindReader
-using HiddenMarkovModelReaders
+# declarations
+begin
+  include("/Users/drivas/Factorem/EEG/src/config/config.jl")
+end;
 
 ################################################################################
 
+# # load project enviroment
+# using Pkg
+# if Pkg.project().path != string(projDir, "/Project.toml")
+#   Pkg.activate(projDir)
+# end
+
+################################################################################
+
+# load packages
+begin
+  using MindReader
+  using HiddenMarkovModelReaders
+end;
+
+################################################################################
+
+# import flux
 import Flux: cpu, gpu, flatten, leakyrelu
 
 ################################################################################
 
+# import parameters
 import Parameters: @with_kw
 
 ################################################################################
 
-# read parameters
-include( "Parameters.jl");
+# load modules
+begin
+  # read parameters
+  include(string(runDataset, "/Parameters.jl"))
 
-################################################################################
-
-include("/Users/drivas/Factorem/EEG/src/annotation/functions/annotationCalibrator.jl")
-include("/Users/drivas/Factorem/EEG/src/annotation/functions/fileReaderXLSX.jl")
+  # load annotation functions
+  include(string(annotationDir, "/functions/annotationCalibrator.jl"))
+  include(string(annotationDir, "/functions/fileReaderXLSX.jl"))
+end;
 
 ################################################################################
 
@@ -49,111 +71,111 @@ dir = "/Users/drivas/Factorem/EEG/data/physionet.org/files/chbmit/1.0.0/chb04/"
 xfile = "chb04-summary.txt"
 file = "chb04_28.edf"
 
-annotFile = annotationReader( string(dir, xfile) )
+annotFile = annotationReader(string(dir, xfile))
 
 ################################################################################
 
 dirRead = readdir(dir)
-fileList = contains.(dirRead, r"edf$") |> p -> getindex(dirRead, p)
+fileList = contains.(dirRead, r"edf$") |> π -> getindex(dirRead, π)
 
 # for file in fileList
 
-  @info file
+@info file
 
-  #  read data
-  begin
-    # read edf file
-    edfDf, startTime, recordFreq = getSignals(shArgs)
+#  read data
+begin
+  # read edf file
+  edfDf, startTime, recordFreq = getSignals(shArgs)
 
-    # calculate fft
-    freqDc = extractFFT(edfDf, shArgs)
-
-    if haskey(annotFile, replace(shArgs["file"], ".edf" => ""))
-
-      labelAr = annotationCalibrator(
-        annotFile[replace(shArgs["file"], ".edf" => "")],
-        startTime = startTime,
-        recordFreq = recordFreq,
-        signalLength = size(edfDf, 1),
-        shParams = shArgs,
-      )
-    end
-  end;
-
-  ################################################################################
-
-  # build autoencoder & train hidden Markov model
-  begin
-
-    # create empty dictionary
-    errDc = Dict{String, Tuple{Array{Int64, 1}, Array{Array{Float64, 1}, 1}}}()
-
-    for (κ, υ) ∈ freqDc
-      println()
-      @info κ
-
-      #  build & train autoencoder
-      freqAr = shifter(υ)
-      model = buildAutoencoder(length(freqAr[1]), nnParams = NNParams)
-      model = modelTrain!(model, freqAr, nnParams = NNParams)
-
-      ################################################################################
-
-      # calculate post autoencoder
-      postAr = cpu(model).(freqAr)
-
-      ################################################################################
-
-      begin
-        @info "Creating Hidden Markov Model..."
-        # error
-        aErr = reshifter(postAr - freqAr) |> π -> flatten(π) |> permutedims
-
-        # setup
-        hmm = setup(aErr)
-
-        # process
-        for _ ∈ 1:4
-          errDc[κ] = process!(hmm, aErr, true, params = hmmParams)
-        end
-
-        # final
-        for _ ∈ 1:2
-          errDc[κ] = process!(hmm, aErr, false, params = hmmParams)
-        end
-      end;
-      ################################################################################
-
-    end
-
-    println()
-
-  end;
-
-  ################################################################################
-
-  # write traceback & states
-  writeHMM(errDc, shArgs)
-
-  ################################################################################
+  # calculate fft
+  freqDc = extractFFT(edfDf, shArgs)
 
   if haskey(annotFile, replace(shArgs["file"], ".edf" => ""))
 
-    scr = sensitivitySpecificity(errDc, labelAr)
+    labelAr = annotationCalibrator(
+      annotFile[replace(shArgs["file"], ".edf" => "")],
+      startTime = startTime,
+      recordFreq = recordFreq,
+      signalLength = size(edfDf, 1),
+      shParams = shArgs,
+    )
+  end
+end;
 
-    writedlm( string(shArgs["outscreen"], replace(shArgs["file"], "edf" => "csv")), writePerformance(scr), ", " )
+################################################################################
+
+# build autoencoder & train hidden Markov model
+begin
+
+  # create empty dictionary
+  errDc = Dict{String, Tuple{Array{Int64, 1}, Array{Array{Float64, 1}, 1}}}()
+
+  for (κ, υ) ∈ freqDc
+    println()
+    @info κ
+
+    #  build & train autoencoder
+    freqAr = shifter(υ)
+    model = buildAutoencoder(length(freqAr[1]), nnParams = NNParams)
+    model = modelTrain!(model, freqAr, nnParams = NNParams)
 
     ################################################################################
 
-    # graphic rendering
-    mindGraphics(errDc, shArgs, labelAr)
+    # calculate post autoencoder
+    postAr = cpu(model).(freqAr)
 
-  else
+    ################################################################################
 
-    # graphic rendering
-    mindGraphics(errDc, shArgs)
+    begin
+      @info "Creating Hidden Markov Model..."
+      # error
+      aErr = reshifter(postAr - freqAr) |> π -> flatten(π) |> permutedims
+
+      # setup
+      hmm = setup(aErr)
+
+      # process
+      for _ ∈ 1:4
+        errDc[κ] = process!(hmm, aErr, true, params = hmmParams)
+      end
+
+      # final
+      for _ ∈ 1:2
+        errDc[κ] = process!(hmm, aErr, false, params = hmmParams)
+      end
+    end
+    ################################################################################
 
   end
+
+  println()
+
+end;
+
+################################################################################
+
+# write traceback & states
+writeHMM(errDc, shArgs)
+
+################################################################################
+
+if haskey(annotFile, replace(shArgs["file"], ".edf" => ""))
+
+  scr = sensitivitySpecificity(errDc, labelAr)
+
+  writedlm(string(shArgs["outscreen"], replace(shArgs["file"], "edf" => "csv")), writePerformance(scr), ", ")
+
+  ################################################################################
+
+  # graphic rendering
+  mindGraphics(errDc, shArgs, labelAr)
+
+else
+
+  # graphic rendering
+  mindGraphics(errDc, shArgs)
+
+end
 
   ################################################################################
 
