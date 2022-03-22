4a485d426a5d82d7016afdd44210408544703a32
DanielRivasMD <danielrivasmd@gmail.com>
2022-03-22 11:35:26 +0100

    dale mas color!

---
+++ src/config/colors.R
@@ -0,0 +1,41 @@
+################################################################################
+
+# color names
+colors <- c('black', 'tomato', 'lightsalmon', 'goldenrod', 'darkolivegreen', 'lightseagreen', 'darkslategray', 'steelblue', 'slateblue', 'lavender')
+
+################################################################################
+
+# RGB
+rgbColors <- matrix(
+  data = c(
+    c(0, 0, 0),
+    c(255, 99, 71),
+    c(255, 160, 122),
+    c(218, 165, 32),
+    c(85, 107, 47),
+    c(32, 178, 170),
+    c(47, 79, 79),
+    c(70, 130, 180),
+    c(106, 90, 205),
+    c(230, 230, 250)
+  ),
+  nrow = 3,
+) %>% t
+
+colnames(rgbColors) <- c('r', 'g', 'b')
+rownames(rgbColors) <- colors
+
+################################################################################
+
+# HEX
+hexColors <- matrix(
+  data = c(
+    '#000000', '#ff6347', '#ffa07a', '#daa520', '#556b2f', '#20b2aa', '#2f4f4f', '#4682b4', '#6a5acd', '#e6e6fa'
+  ),
+  ncol = 1,
+)
+
+colnames(hexColors) <- c('hex')
+rownames(hexColors) <- colors
+
+################################################################################
