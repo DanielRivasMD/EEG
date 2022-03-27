d9bf1124022dec49d1cf0fc8a134fe6a1f68a60a
DanielRivasMD <danielrivasmd@gmail.com>
2022-03-27 21:42:57 +0200

    ignore complex heatmap annotations

--- src/graphics/complexHeatmap.R
+++ src/graphics/complexHeatmap.R
@@ -23,14 +23,15 @@ states <- anomalyHm %>% apply(FUN = table, MARGIN = 2) %>% rownames
 # open plotting device
 pdf(file = paste0(mindPlot, '/sample.pdf'), width = 16, height = 12)
 
-# backup plotting area
-defParMar <- par("mar")
+# # backup plotting area
+# defParMar <- par('mar')
 
-# expand plotting area
-par(mar = c(defParMar[-4], 5))
+# # expand plotting area
+# par(mar = c(defParMar[-4], 5))                              # expand on the right
+# par(mar = c(defParMar[1:2], 10, defParMar[4]))              # expand on top
 
 # plot with image
-anomalyHm %>%
+anomalyHm[1:1000, ] %>%
   as.matrix %>%
   image(
     col = hexColors,
@@ -48,40 +49,43 @@ axis(
   tick = FALSE,
 )
 
-# activate outside plotting area
-par(xpd = NA)
+# # activate outside plotting area
+# par(xpd = NA)
 
-# annotations rectangles
-annotScale <- 1 / length(states)
+# # annotations rectangles
+# annotScale <- 1 / length(states)
 
-for(ix in seq_along(states)) {
-  rect(
-    xleft = (par('usr')[2] * 1.04),
-    ybottom = (par('usr')[4] * (ix - 0.9) * annotScale),
-    xright = (par('usr')[2] * 1.07),
-    ytop = (par('usr')[4] * (ix) * annotScale),
-    border = NA,
-    col = rgb(
-      red = rgbColors[ix, 'r'],
-      green = rgbColors[ix, 'g'],
-      blue = rgbColors[ix, 'b'],
-      max = 255,
-      alpha = 255,
-    )
-  )
+# for(ix in seq_along(states)) {
 
-  # annotation text
-  text(
-    x = (par('usr')[2] * 1.08),
-    y = (par('usr')[4] * (ix - 0.5) * annotScale),
-    label = paste0('State: ', states[ix]),
-    cex = 0.7,
-    adj = 0,
-  )
-}
+#   # annotation rectangles
+#   rect(
+#     xleft = (par('usr')[2] * 1.04),
+#     ybottom = (par('usr')[4] * (ix - 0.9) * annotScale),
+#     xright = (par('usr')[2] * 1.07),
+#     ytop = (par('usr')[4] * (ix) * annotScale),
+#     border = NA,
+#     col = rgb(
+#       red = rgbColors[ix, 'r'],
+#       green = rgbColors[ix, 'g'],
+#       blue = rgbColors[ix, 'b'],
+#       max = 255,
+#       alpha = 255,
+#     )
+#   )
 
-# reset plotting area
-par(mar = defParMar)
+#   # annotation text
+#   text(
+#     x = (par('usr')[2] * 1.08),
+#     y = (par('usr')[4] * (ix - 0.5) * annotScale),
+#     label = paste0('State: ', states[ix]),
+#     cex = 0.7,
+#     adj = 0,
+#   )
+
+# }
+
+# # reset plotting area
+# par(mar = defParMar)
 
 # close plotting device
 dev.off()
