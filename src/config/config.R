089bcb6d0e1f739fd7effb5ba3397fdefc6384c5
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-29 10:57:33 +0200

    update config

--- src/config/config.R
+++ src/config/config.R
@@ -1,23 +1,23 @@
-################################################################################
+####################################################################################################
 
 # project
 projDir <- '/Users/drivas/Factorem/EEG'
-mindDir = '/Users/drivas/Factorem/MindReader'
+mindDir <- '/Users/drivas/Factorem/MindReader'
 
-################################################################################
+####################################################################################################
 
 # source
-binDir = paste0(projDir, '/src/bin')
-configDir = paste0(projDir, '/src/config')
-runDataset = paste0(projDir, '/src/runDataset')
-annotationDir = paste0(projDir, '/src/annotation')
+binDir <- paste0(projDir, '/src/bin')
+configDir <- paste0(projDir, '/src/config')
+runDataset <- paste0(projDir, '/src/runDataset')
+annotationDir <- paste0(projDir, '/src/annotation')
 utilDir <- paste0(projDir, '/src/utilities')
 graphDir <- paste0(projDir, '/src/graphics')
 
-################################################################################
+####################################################################################################
 
 # data
-mindCSV = paste0(mindDir, '/data/csv')
-mindPlot = paste0(mindDir, '/data/plot')
+mindCSV <- paste0(mindDir, '/data/csv')
+mindPlot <- paste0(mindDir, '/data/plot')
 
-################################################################################
+####################################################################################################
