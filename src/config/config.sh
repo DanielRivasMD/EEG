35158ca2b7eab6bc187299d3c7503350057c58b6
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-29 10:57:41 +0200

    add config shell

---
+++ src/config/config.sh
@@ -0,0 +1,26 @@
+####################################################################################################
+
+# project
+projDir="/Users/drivas/Factorem/EEG"
+mindDir="/Users/drivas/Factorem/MindReader"
+
+####################################################################################################
+
+# data
+dataDir="${projDir}/data"
+
+# source
+binDir="${projDir}/src/bin"
+configDir="${projDir}/src/config"
+runDataset="${projDir}/src/runDataset"
+annotationDir="${projDir}/src/annotation"
+utilDir="${projDir}/src/utilities"
+graphDir="${projDir}/src/graphics"
+
+####################################################################################################
+
+# data
+mindCSV="${mindDir}/data/csv"
+mindPlot="${mindDir}/data/plot"
+
+####################################################################################################
