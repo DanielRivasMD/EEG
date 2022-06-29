63b30f6c69bcbb58215f417c5d9f492d43255203
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-29 11:04:45 +0200

    update collect time stampts just protocol

--- src/runDataset/obtainTimestamps.sh
+++ src/runDataset/obtainTimestamps.sh
@@ -8,10 +8,26 @@ source "${HOME}/Factorem/EEG/src/config/config.sh"
 
 ####################################################################################################
 
+# declare database
+database="${dataDir}/chb-mit-scalp-eeg-database-1.0.0.zip"
+
+####################################################################################################
+
+# declare counter
+ct=0
+
 # iterate on files
-for file in $(unzip -l "${dataDir}/chb-mit-scalp-eeg-database-1.0.0.zip" | awk '/edf$/ {print $NF}')
+for file in $(unzip -l "${database}" | awk '/edf$/ {print $NF}')
 do
-  echo "${file}"
+  # log
+  echo "${file/*\/}"
+
+  # increase count
+  ((ct+=1))
+  if [[ ${ct} == 1 ]]
+  then
+    unzip -p "${database}" "${file}" > "${dataDir}/${file/*\/}"
+  fi
 done
 
 ####################################################################################################
