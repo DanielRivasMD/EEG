279fe53b5a052eb6497dc57b299b78a0c2e997e1
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-30 11:14:04 +0200

    draft file iteration for trained models

--- src/runDataset/performanceControl.sh
+++ src/runDataset/performanceControl.sh
@@ -13,33 +13,53 @@ database="${dataDir}/chb-mit-scalp-eeg-database-1.0.0.zip"
 
 ####################################################################################################
 
-# iterate on files
-for file in $(unzip -l "${database}" | awk '/summary/ {print $NF}')
+# iterate on records
+for ix in {1..24}
 do
+
+  # select summary
+  fullsummary="$(unzip -l "${database}" | awk -v ix="$(printf %02d $ix)" '{if ($NF ~ ix && $NF ~ "summary") {print $NF}}')"
+
+  # declare summary
+  summary="${summary/*\/}"
+
   # log
-  echo "${file/*\/}"
-    # extract
-    unzip -p "${database}" "${file}" > "${dataDir}/${file/*\/}"
-done
+  echo "SUMMARY: ${summary}"
 
-####################################################################################################
+  # # extract
+  # unzip -p "${database}" "${fullsummary}" > "${dataDir}/${summary}"
 
-# declare counter
-ct=0
+  ####################################################################################################
 
-# iterate on files
-for file in $(unzip -l "${database}" | awk '/edf$/ {print $NF}')
-do
-  # log
-  echo "${file/*\/}"
+  # iterate on files
+  for fulledf in "$(unzip -l "${database}" | awk -v ix="$(printf %02d $ix)" '{if ($NF  ~ "chb"ix && $NF ~ "edf$") {print $NF}}')"
+  do
 
-  # increase count
-  ((ct+=1))
-  if [[ ${ct} == 1 ]]
-  then
-    # extract
-    unzip -p "${database}" "${file}" > "${dataDir}/${file/*\/}"
-  fi
+    # declare edf
+    edf="${fulledf/*\/}"
+
+    # log
+    echo "EDF: ${edf}"
+    echo "OUT: ${dataDir}/${edf}"
+
+    echo "LOG: /Users/drivas/Factorem/MindReader/data/log/${edf/edf/log}"
+
+    # # extract
+    # unzip -p "${database}" "${fulledf}" > "${dataDir}/${edf}"
+
+    # julia \
+    #   --project \
+    #   "/Users/drivas/Factorem/EEG/src/runDataset/readMind.jl" \
+    #   --input "${edf}" \
+    #   --inputDir "${dataDir}/" \
+    #   --annotation "${summary}" \
+    #   --annotDir "${dataDir}/" \
+    #   --outDir "/Users/drivas/Factorem/MindReader/data/" \
+    #   --additional "annotationCalibrator.jl,fileReaderXLSX.jl" \
+    #   --addDir "/Users/drivas/Factorem/EEG/src/annotation/functions/" &> "/Users/drivas/Factorem/MindReader/data/log/${edf/edf/log}"
+
+  done
+
 done
 
 ####################################################################################################
