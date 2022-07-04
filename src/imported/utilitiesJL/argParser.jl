51a5cff53be8354117cca08f57d09549d5ba5698
DanielRivasMD <danielrivasmd@gmail.com>
2022-07-04 06:21:49 +0200

    update arg parser

--- src/imported/utilitiesJL/argParser.jl
+++ src/imported/utilitiesJL/argParser.jl
@@ -1,51 +1,90 @@
-################################################################################
+####################################################################################################
 
-using ArgParse
+using ArgParse: ArgParseSettings, @add_arg_table!, parse_args
 
-################################################################################
+####################################################################################################
 
 function shArgParser(args)
-  # minimal argument parsing
-  s = ArgParseSettings(description = "mind reader")
-  @add_arg_table! s begin
-      "--file", "-f"
+    # minimal argument parsing
+    ∫ = ArgParseSettings(; description = "MindReader command line utility.")
+
+    @add_arg_table! ∫ begin
+
+        "--input", "-i"
         arg_type = String
         required = true
         help = "`edf` file to read"
-      "--xlsx", "-a"
+
+        "--inputDir", "-I"
+        arg_type = String
+        required = true
+        help = "`edf` file directory"
+
+        "--params", "-p"
+        arg_type = String
+        required = true
+        help = "parameters"
+
+        "--paramsDir", "-P"
+        arg_type = String
+        required = true
+        help = "params file directory"
+
+        "--annotation", "-a"
         arg_type = String
         required = false
-        help = "`xlsx` file to read"
-      "--test-file", "-y"
+        help = "annotation file"
+
+        "--annotDir", "-A"
         arg_type = String
         required = false
-        help = "`edf` file to test"
-      "--test-xlsx", "-x"
+        help = "annotation file directory"
+
+        "--outDir", "-o"
         arg_type = String
         required = false
-        help = "`xlsx` file to test"
-      "--output", "-o"
-        arg_type = String
-        required = false
+        default = "."
         help = "output directory"
-      "--fft", "-t"
+
+        "--additional", "-f"
+        arg_type = String
+        required = false
+        help = "comma-separated list of additional files to include"
+
+        "--addDir", "-F"
+        arg_type = String
+        required = false
+        help = "additional file(s) directory"
+
+        "--window-size", "-w"
         nargs = '?'
         arg_type = Int
-        default = 16
-        help = "number of frequencies for Fourier transform"
-      "--window-size", "-w"
-        nargs = '?'
-        arg_type = Int
-        default = 128
+        default = 256
         help = "window size along raw signal"
-      "--bin-overlap", "-b"
+
+        "--bin-overlap", "-b"
         nargs = '?'
         arg_type = Int
         default = 4
         help = "bin overlap along raw signal"
-  end
-  parsed_args = parse_args(s)
-  return parsed_args
+
+    end
+
+    parsed_args = parse_args(∫)
+    return parsed_args
 end
 
-################################################################################
+####################################################################################################
+
+#  parse shell arguments
+shArgs = shArgParser(ARGS)
+
+####################################################################################################
+
+# # create directory if does not exist
+# if !isdir(shArgs["outdir"]) && shArgs["outdir"] != "."
+#   @info "Creating directory `$(shArgs["outdir"])`"
+#   mkdir(shArgs["outdir"])
+# end
+
+####################################################################################################
