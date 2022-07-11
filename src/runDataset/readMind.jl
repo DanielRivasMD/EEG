9bbef32aa116b3db6dce148bba9df8c7e9c88421
DanielRivasMD <danielrivasmd@gmail.com>
2022-07-11 10:06:50 +0200

    add peak identification function

--- src/runDataset/readMind.jl
+++ src/runDataset/readMind.jl
@@ -18,6 +18,7 @@ begin
   using HiddenMarkovModelReaders
 
   using DelimitedFiles
+  using RCall
 
   # flux
   using Flux: cpu, gpu, flatten, leakyrelu
@@ -44,6 +45,30 @@ if haskey(shArgs, "additional") && haskey(shArgs, "addDir")
     include(string(shArgs["addDir"], ι))
   end
 end
+
+####################################################################################################
+
+begin
+  # declare peak identification function
+  R"
+  peak_iden <- function(
+
+    f_seq,
+    d_threshold = NULL
+  ) {
+
+    if (is.null(d_threshold)) d_threshold <- 1
+    f_seq <- c(0, f_seq, 0)
+    f_threseq <- which(f_seq >= d_threshold)
+    f_peak_length <- which(f_seq[f_threseq + 1] < d_threshold) - which(f_seq[f_threseq-1] < d_threshold) + 1
+    f_upper_lim_ix <- (f_threseq[cumsum(f_peak_length)]) - 1
+    f_lower_lim_ix <- f_upper_lim_ix - f_peak_length + 1
+    peak_feat <- data.frame(peak_no = seq_along(f_lower_lim_ix), lower_lim_ix = f_lower_lim_ix, upper_lim_ix = f_upper_lim_ix, peak_length_ix = f_peak_length)
+
+    return(peak_feat)
+  }
+  "
+end;
 
 ####################################################################################################
 
