9cc28cb9d1f0b0454cf535ae845c7cda4379d629
DanielRivasMD <danielrivasmd@gmail.com>
2022-08-30 12:59:32 +0200

    add physionet run

---
+++ src/runDataset/physionetRun.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# set -euo pipefail
+
+####################################################################################################
+
+# # declare batches
+# arr=(1..6 7..12 13..18 19..24)
+
+# # iterate on patient batches
+# for bt in ${arr[@]}
+# do
+
+  bt=1..6
+
+  # iterate on patients
+  for ix in {$bt}
+  do
+    # call physionet controller
+    source src/runDataset/physionet.sh $(printf "%02d\n" $ix) &
+  done
+# done
+
+####################################################################################################
