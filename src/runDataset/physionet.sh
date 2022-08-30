72d6c6ad9af06da99d5ee6dce08bedb1b707139e
DanielRivasMD <danielrivasmd@gmail.com>
2022-08-30 12:59:22 +0200

    update for external iteration

--- src/runDataset/physionet.sh
+++ src/runDataset/physionet.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # set -euo pipefail
-# DOC: control model training on physionet. call MindReader ReadMind.jl
+# DOC: control model training on physionet through . call MindReader ReadMind.jl
 
 ####################################################################################################
 
 # iterate on patient directories
-for patient in $(command find /Users/drivas/Factorem/EEG/data/physionet.org/files/chbmit/1.0.0/* -type d)
+for patient in $(command find /Volumes/G/EEG/physionet.org/files/chbmit/1.0.0/* -type d -name "chb${1}")
 do
 
   # identify annotation file
@@ -18,6 +18,7 @@ do
     # echo input file
     echo "EDF: ${${edf//*\/}/.edf/}"
 
+    # call read mind
     julia \
       --project \
       "/Users/drivas/Factorem/MindReader/src/ReadMind.jl" \
