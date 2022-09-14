8e1a85be01edadcc9e8f0074e593296a84f6ec76
DanielRivasMD <danielrivasmd@gmail.com>
2022-09-14 18:19:26 +0200

    add extract summaries

---
+++ src/runDataset/extractSummary.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# set -euo pipefail
+
+####################################################################################################
+
+# config
+source "${HOME}/Factorem/EEG/src/config/config.sh"
+
+####################################################################################################
+
+# declarations
+database="${dataDir}/chb-mit-scalp-eeg-database-1.0.0.zip"
+
+####################################################################################################
+
+# iterate on records
+for ix in {1..24}
+do
+
+  # select summary
+  fullsummary="$(unzip -l "${database}" | awk -v ix="$(printf %02d $ix)" '{if ($NF ~ ix && $NF ~ "summary") {print $NF}}')"
+
+  # declare summary
+  summary="${fullsummary/*\/}"
+
+  # log
+  echo "\nSUMMARY: ${summary}\n"
+
+  # extract
+  unzip -p "${database}" "${fullsummary}" > "${dataDir}/${summary}"
+
+done
+
+####################################################################################################
