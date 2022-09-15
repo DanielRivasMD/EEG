bdd467efee57406740c1063469bc5a21b3e4256b
DanielRivasMD <danielrivasmd@gmail.com>
2022-09-15 16:06:36 +0200

    update error run controller

--- src/runDataset/runErrors.sh
+++ src/runDataset/runErrors.sh
@@ -3,41 +3,14 @@
 
 ####################################################################################################
 
-# config
-source "${HOME}/Factorem/EEG/src/config/config.sh"
+# declare batches
+arr=(annotation bounds inexact)
 
-####################################################################################################
-
-# iterate on errors
-for err in $(command find "${mindData}/typeErr/annotation" -type f)
+# iterate on error batches
+for bt in ${arr[@]}
 do
-
-  # declare edf
-  edf="${err/err/edf}"
-
-  # patch directory
-  dir="${${edf//*\/}/_*/_}"
-  dir="${dir/a_/_}"
-  dir="${dir/b_/_}"
-  dir="${dir/_/}"
-
-  # echo input file
-  echo "EDF: ${${edf//*\/}/.edf/}"
-
-  # call read mind
-  julia \
-    --project \
-    "/Users/drivas/Factorem/MindReader/src/ReadMind.jl" \
-    --input "${edf//*\/}" \
-    --inputDir "/Volumes/G/EEG/physionet.org/files/chbmit/1.0.0/${dir}/" \
-    --params "Parameters.jl" \
-    --paramsDir "/Users/drivas/Factorem/EEG/src/runDataset/" \
-    --annotation "${annotation//*\/}" \
-    --annotDir "${patient}/" \
-    --outDir "/Users/drivas/Factorem/MindReader/data/" \
-    --additional "annotationCalibrator.jl,fileReaderXLSX.jl" \
-    --addDir "/Users/drivas/Factorem/EEG/src/annotation/functions/" 1> "/Users/drivas/Factorem/MindReader/data/log/${${edf//*\/}/edf/log}" 2> "/Users/drivas/Factorem/MindReader/data/err/${${edf//*\/}/edf/err}"
-
+  # call error controller
+  source src/runDataset/errors.sh $bt
 done
 
 ####################################################################################################
