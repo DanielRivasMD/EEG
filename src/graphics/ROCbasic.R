7e142a9ac9ff8b3977cc0315b3bec80331956c74
DanielRivasMD <danielrivasmd@gmail.com>
2022-09-19 17:36:35 +0200

    update plot roc basic

--- src/graphics/ROCbasic.R
+++ src/graphics/ROCbasic.R
@@ -13,40 +13,53 @@ require(tidyverse)
 
 ####################################################################################################
 
-for (ƒ in list.files(mindScreen)) {
+# for (ƒ in list.files(mindScreen)) {
+# for (ƒ in list.files(paste0(mindData, '/filterScreen'))) {
+for (ι in 1:24) {
+
+  # adjust digits
+  if (ι < 10) {
+    ƒ <- paste0('chb', '0', ι, '.csv')
+  } else {
+    ƒ <- paste0('chb', ι, '.csv')
+  }
 
   # log
   print(ƒ)
 
   # load file
   csv <- read_csv(paste0(
-    mindScreen, '/', ƒ),
+    paste0(mindData, '/filterScreen'), '/', ƒ),
     show_col_types = FALSE,
   )
 
-  # new device
-  quartz()
+  # check for non-empty dataframe
+  if (dim(csv)[1] > 0) {
 
-  # plot
-  plot(
-    x = (1 - csv[['Specificity']]),
-    y = csv[['Sensitivity']],
-    pch = 16,
-    col = 'navyblue',
-    xlim = c(0, 1),
-    ylim = c(0, 1),
-    las = 1,
-    xlab = 'False Positive Rate',
-    ylab = 'True Positive Rate',
-    main = 'Receiver Operating Characteristic (ROC) curve',
-    sub = ƒ %>% str_replace('.csv', ''),
-  )
+    # new device
+    quartz()
 
-  lines(
-    x = 0:1,
-    y = 0:1,
-    col = 'red',
-  )
+    # plot
+    plot(
+      x = (1 - csv[['Specificity']]),
+      y = csv[['Sensitivity']],
+      pch = 16,
+      col = 'navyblue',
+      xlim = c(0, 1),
+      ylim = c(0, 1),
+      las = 1,
+      xlab = 'False Positive Rate',
+      ylab = 'True Positive Rate',
+      main = 'Receiver Operating Characteristic (ROC) curve',
+      sub = ƒ %>% str_replace('.csv', ''),
+    )
+
+    lines(
+      x = 0:1,
+      y = 0:1,
+      col = 'red',
+    )
+  }
 }
 
 ####################################################################################################
