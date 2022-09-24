0474fb9d59f109963fa9a9e26210e781fb8c0580
DanielRivasMD <danielrivasmd@gmail.com>
2022-09-24 17:01:35 +0200

    patch concatenated filter performance shell

--- src/bin/shell/performanceConcatenatedFilter.sh
+++ src/bin/shell/performanceConcatenatedFilter.sh
@@ -10,6 +10,7 @@ source "${HOME}/Factorem/EEG/src/config/config.sh"
 ####################################################################################################
 
 # declarations
+database="${dataDir}/chb-mit-scalp-eeg-database-1.0.0.zip"
 scriptJL="${binDir}/julia/performanceConcatenatedFilter.jl"
 
 ####################################################################################################
@@ -18,11 +19,24 @@ scriptJL="${binDir}/julia/performanceConcatenatedFilter.jl"
 for ix in {1..24}
 do
 
+  # select summary
+  fullsummary="$(unzip -l "${database}" | awk -v ix="$(printf %02d $ix)" '{if ($NF ~ ix && $NF ~ "summary") {print $NF}}')"
+
+  # declare summary
+  summary="${fullsummary/*\/}"
+
+  # log
+  echo "\nSUMMARY: ${summary}\n"
+
+  # extract
+  unzip -p "${database}" "${fullsummary}" > "${dataDir}/${summary}"
+
+  ####################################################################################################
+
   # calculate performance
   julia \
     --project \
     "${scriptJL}" \
-    --inputDir "${dataDir}/" \
     --params "Parameters.jl" \
     --paramsDir "${binDir}/julia/" \
     --annotation "${summary}" \
