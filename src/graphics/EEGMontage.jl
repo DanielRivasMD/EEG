418750a502f777263492e2e454e6ce4b8723deb8
DanielRivasMD <danielrivasmd@gmail.com>
2022-09-27 13:41:10 +0200

    update automathic eeg montage

--- src/graphics/EEGMontage.jl
+++ src/graphics/EEGMontage.jl
@@ -17,13 +17,16 @@ end
 
 # load packages
 begin
-  using Images
+  using Chain: @chain
 
   # ai models
   using AImodels
 
   # Makie
   using CairoMakie
+
+  # images
+  using Images
 end;
 
 ####################################################################################################
@@ -74,16 +77,6 @@ df = hcat(df...)
 
 ####################################################################################################
 
-# # sample row 1215
-# # DataFrameRow
-# #   Row │ C3-P3    C4-P4    CZ-PZ    F3-C3    F4-C4    F7-T7    F8-T8    FP1-F3   FP1-F7   FP2-F4   FP2-F8   FT10-T8  FT9-FT10  FZ-CZ    P3-O1    P4-O2    P7-O1    P7-T7    P8-O2    T7-FT9   T7-P7    T8-P8
-# #       │ Float64  Float64  Float64  Float64  Float64  Float64  Float64  Float64  Float64  Float64  Float64  Float64  Float64   Float64  Float64  Float64  Float64  Float64  Float64  Float64  Float64  Float64
-# # ──────┼───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
-# #  1215 │     3.0      1.0      1.0      4.0      3.0      2.0      2.0      2.0      5.0      2.0      3.0      2.0       2.0      2.0      2.0      2.0      2.0      2.0      2.0      2.0      3.0      2.0
-# sampleRow = [3.0, 1.0, 1.0, 4.0, 3.0, 2.0, 2.0, 2.0, 5.0, 2.0, 3.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 3.0, 2.0]
-
- ####################################################################################################
-
 # load image & permute dimensions
 img = load("assets/EEGMontage.png") |> permutedims |> π -> π[:, end:-1:1]
 
@@ -117,6 +110,8 @@ extractPoints = [1215, 2800, 5000]
 # iterate on extract points
 for ι ∈ extractPoints
 
+  ####################################################################################################
+
   # BUG: electrodes is an unipolar dictionary
   # use arbitrary electrodes to generate image
   ç = 0
@@ -130,36 +125,31 @@ for ι ∈ extractPoints
     toHeat[υ[1]:υ[1] + rangeSize, υ[2] - rangeSize:υ[2]] .= montageβ[υ[1]:υ[1] + rangeSize, υ[2] - rangeSize:υ[2]] .* conicMask .* df[ι, ç]
   end
 
-  # write
-  writedlm(
-    string(mindCSV, "/", "EEGMontage", ι, ".csv"),
-    toHeat,
-    ',',
-  )
+  ####################################################################################################
 
-end
+  # normalize by min-max
+  normHeat = minMax(toHeat)
 
-####################################################################################################
+  # clone image
+  heatβ = copy(img)
 
-# normalize by min-max
-normHeat = minMax(toHeat)
-
-# clone image
-heatβ = copy(img)
-
-# iterate on rows & cols
-for ι ∈ eachindex(eachrow(heatβ)), ο ∈ eachindex(eachcol(heatβ))
-  # substitute electrode positions
-  if toHeat[ι, ο] > 0
-    val = abs(normHeat[ι, ο] - 1)
-    val > 1 && @info ι, ο, normHeat[ι, ο]
-    heatβ[ι, ο] = RGBA(val, val, val, img[ι, ο].alpha)
+  # iterate on rows & cols
+  for ι ∈ eachindex(eachrow(heatβ)), ο ∈ eachindex(eachcol(heatβ))
+    # substitute electrode positions
+    if toHeat[ι, ο] > 0
+      val = abs(normHeat[ι, ο] - 1)
+      val > 1 && @info ι, ο, normHeat[ι, ο]
+      heatβ[ι, ο] = RGBA(val, val, val, img[ι, ο].alpha)
+    end
   end
+
+  ####################################################################################################
+
+  # render image
+  renderImg(heatβ, string(dataDir, "/", "heatmap", ι, ".svg"))
+
+  ####################################################################################################
+
 end
-
-####################################################################################################
-
-# render image
-renderImg(heatβ, "data/img.svg")
 
 ####################################################################################################
