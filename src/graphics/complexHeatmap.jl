db00cda4bdfb4d0402052d1618dffd690cf6c71d
DanielRivasMD <danielrivasmd@gmail.com>
2022-09-28 18:38:16 +0200

    apply post processing filter

--- src/graphics/complexHeatmap.jl
+++ src/graphics/complexHeatmap.jl
@@ -10,6 +10,7 @@ end;
 # load packages
 begin
   using ImageTransformations
+  using RCall
 
   # Makie
   using CairoMakie
@@ -41,6 +42,41 @@ df = hcat(df...)
 
 # write dataframe
 writedf(string(mindCSV, "/chb04_28.csv"), df, ',')
+
+####################################################################################################
+
+# load peak identification function
+R" source(paste0($utilDir, '/peakIden.R')) "
+
+####################################################################################################
+
+# declare artificial state
+artificialState = 10.
+
+# since sample per record = 256, window size = 256, & overlap = 4
+# then each bin represents 1 second of recording with 1 quarter of second offset
+# declare time threshold
+timeThres = 40
+
+# apply filter
+for ι ∈ 1:size(df, 2)
+
+  # declare traceback
+  ψ = df[:, ι]
+
+  # identify peak
+  R" peakDf <- peak_iden($ψ, 2) "
+  @rget peakDf
+
+  # reset traceback
+  df[!, ι] = ones(size(df, 1))
+
+  # assign peak values
+  for ρ ∈ eachrow(filter(:peak_length_ix => χ -> χ >= timeThres, peakDf))
+    df[Int(ρ[:lower_lim_ix]):Int(ρ[:upper_lim_ix]), ι] .= artificialState
+  end
+
+end
 
 ####################################################################################################
 
