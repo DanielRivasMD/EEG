5dcfe81d42397de2b3fadc167945665f4cbb5572
DanielRivasMD <danielrivasmd@gmail.com>
2022-10-02 15:42:28 +0200

    add zero filter time threshold

--- src/bin/julia/performanceConcatenatedFilter.jl
+++ src/bin/julia/performanceConcatenatedFilter.jl
@@ -59,7 +59,7 @@ artificialState = 10.
 # since sample per record = 256, window size = 256, & overlap = 4
 # then each bin represents 1 second of recording with 1 quarter of second offset
 # declare time threshold
-timeThresholds = [120, 100, 80, 60, 40, 20]
+timeThresholds = [120, 100, 80, 60, 40, 20, 0]
 
 ####################################################################################################
 
