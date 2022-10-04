df6e3729d3b153f971e4a92b47dde93aeb559dbd
DanielRivasMD <danielrivasmd@gmail.com>
2022-10-04 11:41:08 +0200

    add draft parse performance

--- src/bin/julia/parsePerformance.jl
+++ src/bin/julia/parsePerformance.jl
@@ -11,49 +11,25 @@ end;
 begin
   using CSV
   using DataFrames
-
-  # Makie
-  using CairoMakie
 end
 
 ####################################################################################################
 
-"wrapper over Makie scatter"
-function renderROC(df, out)
-
-  # declare figure
-  φ = Figure()
-
-  # customize layout
-  gl = φ[1, 1] = GridLayout()
-  render = MakieLayout.Axis(
-    gl[1, 1],
-    title = "Receiver Operating Characteristic (ROC) curve",
-    xlabel = "False Positive Rate",
-    ylabel = "True Positive Rate",
-    xticks = (0:0.1:1, string.(0:10:100, "%")),
-    yticks = (0:0.1:1, string.(0:10:100, "%")),
-  )
-
-  # render image
-  scatter!(render, 1 .- df[:, :Specificity], df[:, :Sensitivity])
-
-  # limits
-  xlims!(render, (0, 1))
-  ylims!(render, (0, 1))
-
-  # save figure
-  save(out, φ)
-
-end
+# load modules
+begin
+  include(string(utilDir, "/ioDataFrame.jl"))
+end;
 
 ####################################################################################################
+
+# declare collected dataframe
+sensDf = DataFrame(Electrode = String[])
 
 # list directories
 rocList = readdir(string(mindROC))
 
-# iterate on directories
-for tier ∈ rocList
+# # iterate on directories
+# for tier ∈ rocList
 
   # list records
   csvList = readdir(string(mindROC, "/", tier))
@@ -64,10 +40,14 @@ for tier ∈ rocList
     # read csv file
     df = CSV.read(string(mindROC, "/", tier, "/", csv), DataFrame)
 
-    # plot
-    renderROC(df, string(mindPlot, "/", tier, "/", replace(csv, ".csv" => ""), ".png"))
+    # remove missing rows by index
+    df = df[Not(ismissing.(df[:, :Electrode])), :]
+
+    # join dataframes
+    global sensDf = outerjoin(sensDf, df[:, [:Electrode, :Sensitivity]]; on = :Electrode)
+    rename!(sensDf, :Sensitivity => Symbol(replace(csv, ".csv" => "")))
 
   end
-end
+# end
 
 ####################################################################################################
