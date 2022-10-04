60d67e17f921492429f449e09a256599eeb74797
DanielRivasMD <danielrivasmd@gmail.com>
2022-10-04 14:52:00 +0200

    add patch channel train model & traceback

---
+++ src/bin/shell/patchChannel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# set -euo pipefail
+# DOC: patch wrongly label channel from edf files
+
+####################################################################################################
+
+# config
+source "${HOME}/Factorem/EEG/src/config/config.sh"
+
+####################################################################################################
+
+# patch models
+mv -v "${mindHMM}/chb12_28_01_model.csv" "${mindHMM}/chb12_28_O1_model.csv"
+mv -v "${mindHMM}/chb12_29_01_model.csv" "${mindHMM}/chb12_29_O1_model.csv"
+
+# patch tracebacks
+mv -v "${mindHMM}/chb12_28_01_traceback.csv" "${mindHMM}/chb12_28_O1_traceback.csv"
+mv -v "${mindHMM}/chb12_29_01_traceback.csv" "${mindHMM}/chb12_29_O1_traceback.csv"
+
+####################################################################################################
