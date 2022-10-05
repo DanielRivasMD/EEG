cb9c05e78fb7c174443a0f5b5a194083e72888ed
DanielRivasMD <danielrivasmd@gmail.com>
2022-10-05 16:32:09 +0200

    patch alternative montages

--- src/bin/julia/performanceConcatenate.jl
+++ src/bin/julia/performanceConcatenate.jl
@@ -84,15 +84,16 @@ end
 # patch patient 12 containing different montages
 ####################################################################################################
 
-# select unipolar
-unipolar12 = @chain channels begin
+# select unipolar. chb12_28 & chb12_29
+unipolar = @chain channels begin
   filter(χ -> !contains(χ, "-"), _)
 end
 
 ####################################################################################################
 
-# second bipokar set chb12
-secBipolar12 = ["F7-CS2", "T7-CS2", "P7-CS2", "FP1-CS2", "F3-CS2", "C3-CS2", "P3-CS2", "O1-CS2", "FZ-CS2", "CZ-CS2", "PZ-CS2", "FP2-CS2", "F4-CS2", "C4-CS2", "P4-CS2", "O2-CS2", "F8-CS2", "T8-CS2", "P8-CS2", "C2-CS2", "C6-CS2", "CP2-CS2", "CP4-CS2", "CP6-CS2"]
+# second bipokar set. chb12_27
+staticBipolar = ["F7-CS2", "T7-CS2", "P7-CS2", "FP1-CS2", "F3-CS2", "C3-CS2", "P3-CS2", "O1-CS2", "FZ-CS2", "CZ-CS2", "PZ-CS2", "FP2-CS2", "F4-CS2", "C4-CS2", "P4-CS2", "O2-CS2", "F8-CS2", "T8-CS2", "P8-CS2", "C2-CS2", "C6-CS2", "CP2-CS2", "CP4-CS2", "CP6-CS2"]
+secBipolar = channels[channels .∈ [staticBipolar]]
 
 ####################################################################################################
 
@@ -101,9 +102,8 @@ channels = channels[channels .∉ [[unipolar12; secBipolar12]]]
 
 ####################################################################################################
 
-# declare master datatypes
-msLabelAr = Vector{Int64}(undef, 0)
-msHmmDc = Dict{String, HMM}(χ => HMM(Array{Float64}(undef, 0), Array{Float64}(undef, 0), Array{Int64}(undef, 0)) for χ = channels)
+# patch channels
+mainBipolar = channels[channels .∉ [[unipolar; secBipolar; refChannels]]]
 
 ####################################################################################################
 
