d6a56a5a09f0509770405014ac931d78cf860d65
DanielRivasMD <danielrivasmd@gmail.com>
2022-10-10 18:56:02 +0200

    add summarize performance. read data

---
+++ src/stats/summarizePerformance.jl
@@ -0,0 +1,45 @@
+####################################################################################################
+
+# declarations
+begin
+  include("/Users/drivas/Factorem/EEG/src/config/config.jl")
+end;
+
+####################################################################################################
+
+# load packages
+begin
+  using CSV
+  using DataFrames
+end
+
+####################################################################################################
+
+# load modules
+begin
+  include(string(utilDir, "/ioDataFrame.jl"))
+end;
+
+####################################################################################################
+
+# performance
+performance = [:Sensitivity, :Specificity]
+
+# list directories
+rocList = readdir(string(mindROC))
+
+# iterate on directories
+for tier ∈ rocList
+
+  # iterate on performance
+  for Π ∈ performance
+
+    # read dataframe
+    @eval dir = $(string(Π)) |> lowercase
+    @eval df = readdf(string(mindData, "/", dir, "/", "filter", $tier, ".csv"), ',')
+
+  end
+
+end
+
+####################################################################################################
