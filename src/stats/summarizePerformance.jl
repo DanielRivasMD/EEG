12bbcc783ba4aa4d70ce33ce343010a004e1a56a
DanielRivasMD <danielrivasmd@gmail.com>
2022-10-10 18:56:22 +0200

    add logging summarize performance

--- src/stats/summarizePerformance.jl
+++ src/stats/summarizePerformance.jl
@@ -31,12 +31,22 @@ rocList = readdir(string(mindROC))
 # iterate on directories
 for tier ∈ rocList
 
+  # log
+  @info tier
+
   # iterate on performance
   for Π ∈ performance
 
     # read dataframe
     @eval dir = $(string(Π)) |> lowercase
     @eval df = readdf(string(mindData, "/", dir, "/", "filter", $tier, ".csv"), ',')
+
+    # log
+    @info dir
+
+    # log
+    @info describe(Df)
+    @info Df
 
   end
 
