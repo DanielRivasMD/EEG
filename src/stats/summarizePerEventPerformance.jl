cb7b6a0e1d12770871cca7a80f2ed152e5c45a79
DanielRivasMD <danielrivasmd@gmail.com>
2022-10-14 18:57:18 +0200

    add summarize per event performance

---
+++ src/stats/summarizePerEventPerformance.jl
@@ -0,0 +1,63 @@
+####################################################################################################
+
+# declarations
+begin
+  include("/Users/drivas/Factorem/EEG/src/config/config.jl")
+end;
+
+####################################################################################################
+
+# load packages
+begin
+  using CSV
+  using DataFrames
+  using Statistics
+end;
+
+####################################################################################################
+
+# load modules
+begin
+  include(string(utilDir, "/ioDataFrame.jl"))
+end;
+
+####################################################################################################
+
+# list directories
+rocList = readdir(string(mindData, "/", "event"))
+
+# iterate on directories
+for tier ∈ rocList
+
+  # log
+  @info tier
+
+  # read dataframe
+  df = readdf(string(mindData, "/", "recall", "/", "filter", tier, ".csv"), ',')
+
+  # patch missing values
+  for (ι, ç) ∈ enumerate(eachcol(df))
+    df[!, ι] .= replace(ç, "missing" => missing)
+  end
+
+  # construct dataframe
+  Df = describe(df[:, Not(:Electrode)])
+
+  # calculate standard deviation
+  Df[:, :std] .= map(eachcol(df[:, Not(:Electrode)])) do ç
+    std(skipmissing(ç))
+  end
+
+  # supress type column
+  Df = Df[:, Not(:eltype)]
+
+  # log
+  @info describe(Df)
+  @info Df
+
+  # write dataframe
+  writedf(string(mindData, "/", "summary", "/", "recall", tier, ".csv"), Df, ',')
+
+end
+
+####################################################################################################
