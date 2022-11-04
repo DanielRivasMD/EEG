0019cf91c1f3a91ae03830e62201e28909020d74
DanielRivasMD <danielrivasmd@gmail.com>
2022-11-04 17:04:23 +0100

    update heatmap all recordings

--- src/graphics/sampleHeatmap.jl
+++ src/graphics/sampleHeatmap.jl
@@ -37,137 +37,164 @@ R" source(paste0($utilDir, '/peakIden.R')) ";
 
 ####################################################################################################
 
-# declare recording
-record = "chb04_28"
-subject = "chb04"
+# declare variables
+sizeW = 256
+overW = 4
 
-record = "chb12_23"
-subject = "chb12"
+# subjects
+subjectList = string.("chb", string.(1:24, pad = 2))
 
-record = "chb12_27"
-subject = "chb12"
-
-record = "chb12_29"
-subject = "chb12"
-
-record = "chb24_04"
-subject = "chb24"
-
-record = "chb24_14"
-subject = "chb24"
+database = string(externalDir, "/physionet.org/files/chbmit/1.0.0")
 
 ####################################################################################################
 
-# read edf file
-edfDf, startTime, recordFreq = getSignals(string(dataDir, "/", record, ".edf"))
+# iterate on subjects
+for subject ∈ subjectList
 
-####################################################################################################
+  # log
+  @info subject
 
-# read annotation
-annotFile = annotationReader(string(dataDir, "/"), string(subject, "-summary.txt"))
+  ####################################################################################################
 
-####################################################################################################
+  # read annotation
+  annotFile = annotationReader(string(dataDir, "/"), string(subject, "-summary.txt"))
 
-# calibrate annotations
-labelDf = annotationCalibrator(
-  annotFile[record];
-  recordFreq = recordFreq,
-  signalLength = size(edfDf, 1),
-  shParams = Dict("window-size" => 256, "bin-overlap" => 4),
-) |> π -> DataFrame(Annotation = π)
-
-####################################################################################################
-
-# identify files to load
-states = @chain begin
-  readdir(mindHMM)
-  filter(χ -> occursin(record, χ), _)
-  filter(χ -> occursin("traceback", χ), _)
-end
-
-####################################################################################################
-
-# read files into dataframe array & concatenate
-df = [readdf(string(mindHMM, "/", ι); sep = ',') for ι ∈ states]
-df = hcat(df..., labelDf)
-
-####################################################################################################
-
-# write dataframe
-writedf(string(mindCSV, "/", record, ".csv"), df; sep = ',')
-
-####################################################################################################
-
-# declare artificial state
-artificialState = 10.
-
-# since sample per record = 256, window size = 256, & overlap = 4
-# then each bin represents 1 second of recording with 1 quarter of second offset
-# declare time threshold
-timeThres = 0
-
-# apply filter
-for ι ∈ axes(df, 2)
-
-  # preserve annotation filtering & adjust values
-  if ι == size(df, 2)
-    df[df[:, :Annotation] .== 1, ι] .= artificialState
-    df[df[:, :Annotation] .== 0, ι] .+= 1
-    continue
+  recordList = @chain begin
+    readdir(string(database, "/", subject))
+    filter(χ -> contains(χ, r"edf$"), _)
+    replace.(".edf" => "")
   end
 
-  # declare traceback
-  ψ = df[:, ι]
+  # iterate on records
+  for record ∈ recordList
 
-  # identify peak
-  R" peakDf <- peakIden($ψ, 2) "
-  @rget peakDf
+    # log
+    @info record
 
-  # reset traceback
-  df[!, ι] = ones(size(df, 1))
+    ####################################################################################################
 
-  # assign peak values
-  for ρ ∈ eachrow(filter(:peakLengthIx => χ -> χ >= timeThres, peakDf))
-    df[Int(ρ[:lowerLimIx]):Int(ρ[:upperLimIx]), ι] .= artificialState
+    # read edf file
+    edfDf, startTime, recordFreq = getSignals(string(database, "/", subject, "/", record, ".edf"))
+
+    ####################################################################################################
+
+    # calibrate annotations
+    if haskey(annotFile, record)
+      labelDf = annotationCalibrator(
+        annotFile[record];
+        recordFreq = recordFreq,
+        signalLength = size(edfDf, 1),
+        shParams = Dict("window-size" => sizeW, "bin-overlap" => overW),
+      ) |> π -> DataFrame(Annotation = π)
+    # declare an empty vector
+    else
+      labelDf = zeros(Int, convert.(Int, size(edfDf, 1) / (sizeW / overW))) |> π -> DataFrame(Annotation = π)
+    end
+
+    ####################################################################################################
+
+    # identify files to load
+    states = @chain begin
+      readdir(mindHMM)
+      filter(χ -> occursin(record, χ), _)
+      filter(χ -> occursin("traceback", χ), _)
+      filter(χ -> !contains(χ, "VNS"), _)
+      filter(χ -> !contains(χ, "EKG"), _)
+      filter(χ -> !contains(χ, "LOC"), _)
+      filter(χ -> !contains(χ, "LUE"), _)
+      filter(χ -> !contains(χ, "_-_"), _)
+      filter(χ -> !contains(χ, "_._"), _)
+    end
+
+    ####################################################################################################
+
+    # read files into dataframe array & concatenate
+    df = [readdf(string(mindHMM, "/", ι); sep = ',') for ι ∈ states]
+    df = hcat(df..., labelDf)
+
+    ####################################################################################################
+
+    # write dataframe
+    writedf(string(mindCSV, "/", record, ".csv"), df; sep = ',')
+
+    ####################################################################################################
+
+    # declare artificial state
+    artificialState = 10.
+
+    # since sample per record = 256, window size = 256, & overlap = 4
+    # then each bin represents 1 second of recording with 1 quarter of second offset
+    # declare time threshold
+    timeThres = 0
+
+    # apply filter
+    for ι ∈ axes(df, 2)
+
+      # preserve annotation filtering & adjust values
+      if ι == size(df, 2)
+        df[df[:, :Annotation] .== 1, ι] .= artificialState
+        df[df[:, :Annotation] .== 0, ι] .+= 1
+        continue
+      end
+
+      # declare traceback
+      ψ = df[:, ι]
+
+      # identify peak
+      R" peakDf <- peakIden($ψ, 2) "
+      @rget peakDf
+
+      # reset traceback
+      df[!, ι] = ones(size(df, 1))
+
+      # assign peak values
+      for ρ ∈ eachrow(filter(:peakLengthIx => χ -> χ >= timeThres, peakDf))
+        df[Int(ρ[:lowerLimIx]):Int(ρ[:upperLimIx]), ι] .= artificialState
+      end
+
+    end
+
+    ####################################################################################################
+
+    # plot heatmap
+    φ = Figure()
+
+    # assign axes labels
+    ξ = CairoMakie.Axis(
+      φ[1, 1],
+      title = "Heatmap representing all channels during length of recording",
+      xlabel = "Time along EEG recording",
+      yticks = (1:size(df, 2), df |> names),
+      xticksvisible = false,
+      xticklabelsvisible = false,
+    )
+
+    # plot matrix
+    heatmap!(
+      ξ,
+      df |> Matrix |> π -> imresize(π, (Int(size(df, 1) / 4), size(df, 2))),
+      colormap = ["#ffffff", "#ff0000"],
+    )
+
+    # save figure
+    save(string(mindHeatmap, "/", record, "_", timeThres, ".svg"), φ)
+
+    ####################################################################################################
+
   end
 
 end
 
 ####################################################################################################
 
-# plot heatmap
-φ = Figure()
+# # interactive annotations
+# annot = df[:, :Annotation]
+# R" peakDf <- peakIden($annot, 2) ";
+# @rget peakDf
 
-# assign axes labels
-ξ = CairoMakie.Axis(
-  φ[1, 1],
-  title = "Heatmap representing all channels during length of recording",
-  xlabel = "Time along EEG recording",
-  yticks = (1:size(df, 2), df |> names),
-  xticksvisible = false,
-  xticklabelsvisible = false,
-)
-
-# plot matrix
-heatmap!(
-  ξ,
-  df |> Matrix |> π -> imresize(π, (Int(size(df, 1) / 4), size(df, 2))),
-  colormap = ["#ffffff", "#ff0000"],
-)
-
-# save figure
-save(string(mindPlot, "/", record, "_", timeThres, ".svg"), φ)
-
-####################################################################################################
-
-# interactive annotations
-annot = df[:, :Annotation]
-R" peakDf <- peakIden($annot, 2) ";
-@rget peakDf
-
-# iterate on peaks
-for ρ ∈ eachrow(peakDf)
-  @info ((map(sum, eachrow(df[Int(ρ.lowerLimIx):Int(ρ.upperLimIx), 1:end - 1])) ./ (size(df, 2) - 1)) |> sum) ./ ρ.peakLengthIx
-end
+# # iterate on peaks
+# for ρ ∈ eachrow(peakDf)
+#   @info ((map(sum, eachrow(df[Int(ρ.lowerLimIx):Int(ρ.upperLimIx), 1:end - 1])) ./ (size(df, 2) - 1)) |> sum) ./ ρ.peakLengthIx
+# end
 
 ####################################################################################################
