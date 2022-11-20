4a6ed064b35c56b9a182ef4bedc57f09f033fac3
DanielRivasMD <danielrivasmd@gmail.com>
2022-11-20 17:43:27 +0100

    patch time thresholds

--- src/config/timeThresholds.jl
+++ src/config/timeThresholds.jl
@@ -3,6 +3,7 @@
 # since sample per record = 256, window size = 256, & overlap = 4
 # then each bin represents 1 second of recording with 1 quarter of second offset
 # declare time threshold
-timeThresholds = [120, 100, 80, 60, 40, 20, 0, -720, -960, -1200, -1680, -2400]
+# timeThresholds = [120, 100, 80, 60, 40, 20, 0, -720, -960, -1200, -1680, -2400]
+timeThresholds = [0]
 
 ####################################################################################################
